21f4dc74f79f89ed887e3ac6a7278578888fcaf4
Shaunabh Bose <56901126+Marcus-7258@users.noreply.github.com>
2022-07-20 14:22:13 -0700

    Update Tabletop_robot.ino

--- Tabletop_robot.ino
+++ Tabletop_robot.ino
@@ -1,41 +1,41 @@
 #include <Servo.h>
-const int rightSensorPin = PB3;
-const int leftSensorPin = PB2;
+const byte rightSensorPin = PB3;
+const byte leftSensorPin = PB2;
+const byte backSensorPin = PB4;
 
 Servo rightservo;
 Servo leftservo;
-int pos1 = 0;
-int pos2 = 0;
-int rightSensorReading = 0;
-int leftSensorReading = 0;
-void setup() 
+byte pos1 = 0;
+byte pos2 = 0;
+byte rightSensorReading = 0;
+byte leftSensorReading = 0;
+byte backSensorReading = 0;
+void setup()
 // put your setup code here, to run once:
 {
   rightservo.attach(PB1);
   leftservo.attach(PB0);
-  pinMode(13, OUTPUT);
   pinMode(leftSensorPin, INPUT);
   pinMode(rightSensorPin, INPUT);
+  pinMode(backSensorPin, INPUT);
 }
 void normalmove()
 {
   pos1 = 45;
   pos2 = 135;
   rightservo.write(pos1);
-  leftservo.write(pos2); 
+  leftservo.write(pos2);
 }
 void loop()
-// put your main code here, to run repeatedly:
 {
   leftSensorReading = digitalRead(leftSensorPin);
   rightSensorReading = digitalRead(rightSensorPin);
+  backSensorReading = digitalRead(backSensorPin);
   delay(10);
-  if(rightSensorReading == 0 && leftSensorReading == 0) {
-    //both sensor on table
+  if (!rightSensorReading && !leftSensorReading) {
     normalmove();
   }
-  else if (rightSensorReading == 1 && leftSensorReading == 1) {
-    //both sensor off table
+  else if (rightSensorReading && leftSensorReading && !backSensorReading) {
     pos1 = 135;
     pos2 = 45;
     rightservo.write(pos1);
@@ -43,18 +43,16 @@ void loop()
     delay(500);
     normalmove();
   }
-  else if(rightSensorReading == 1 && leftSensorReading == 0){
-    //right sensor off table
+  else if (rightSensorReading && !leftSensorReading) {
     pos2 = 45;
     pos1 = 45;
     rightservo.write(pos1);
     leftservo.write(pos2);
     delay(500);
     normalmove();
-    
+
   }
-  else if (rightSensorReading == 0 && leftSensorReading == 1){
-    //left sensor off table
+  else if (!rightSensorReading && leftSensorReading) {
     pos2 = 135;
     pos1 = 135;
     rightservo.write(pos1);
@@ -62,10 +60,14 @@ void loop()
     delay(500);
     normalmove();
   }
-  //digitalWrite(13, HIGH);    
-  //digitalWrite(12, HIGH);
+  else if (rightSensorReading && leftSensorReading && backSensorReading)
+  {
+    pos1 = 45;
+    pos2 = 135;
+    rightservo.write(pos1);
+    leftservo.write(pos2);
+    normalmove();
+  }
   rightservo.write(pos1);
   leftservo.write(pos2);
-  //Serial.print(pos2);
-  
 }
